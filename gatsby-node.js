32f2ae4d2d1a37fb680eb7ff4c824572fee0d43e
crs2here <crs2here@gmail.com>
2018-08-11 13:15:13 -0700

    remove starter code

--- gatsby-node.js
+++ gatsby-node.js
@@ -26,7 +26,6 @@ exports.createPages = ({ graphql, actions }) => {
 
   return new Promise((resolve, reject) => {
     const postPage = path.resolve('src/templates/post.js');
-    const categoryPage = path.resolve('src/templates/category.js');
     resolve(
       graphql(`
         {
@@ -63,26 +62,6 @@ exports.createPages = ({ graphql, actions }) => {
               slug: edge.node.fields.slug,
               prev,
               next,
-            },
-          });
-        });
-
-        let categories = [];
-
-        _.each(posts, edge => {
-          if (_.get(edge, 'node.frontmatter.category')) {
-            categories = categories.concat(edge.node.frontmatter.category);
-          }
-        });
-
-        categories = _.uniq(categories);
-
-        categories.forEach(category => {
-          createPage({
-            path: `/categories/${_.kebabCase(category)}`,
-            component: categoryPage,
-            context: {
-              category,
             },
           });
         });
